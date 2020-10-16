cf7959d5b6a52b31b2b67572332a8c096a007db0
Kanstantsin Sakavets <kanstantsin.sakavets@gmail.com>
2020-10-16 22:18:58 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    function strChecker(string, config) {
+        let isIncludes = false;
+        config.forEach(element => {
+            let joinedEl = element.join('');
+            if (string.includes(joinedEl)) {
+                isIncludes = true;
+            }
+        });
+        return isIncludes;
+    }
+
+    do {
+        bracketsConfig.forEach(element => {
+            let joinedEl = element.join('');
+            while (str.includes(joinedEl)) {
+                str = str.replace(joinedEl, '');
+            }
+        });
+    } while (strChecker(str, bracketsConfig));
+
+    if (str.length === 0) {
+        return true;
+    } else return false;
 }
